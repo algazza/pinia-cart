792e7fdb6b3aa5b0501cacef12d84ce0ed088780
algazza <fadhilaswadira72564@gmail.com>
2025-12-01 13:17:50 +0700

    💄ui: update primevue theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,20 @@
 import tailwindcss from "@tailwindcss/vite";
+import Nora from '@primeuix/themes/nora';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  css: ["./app/assets/css/main.css"],
   vite: {
-    plugins: [
-      tailwindcss()
-    ]
+    plugins: [tailwindcss()],
   },
-  modules: ['@nuxt/image', '@primevue/nuxt-module'],
-})
+  modules: ["@nuxt/image", "@primevue/nuxt-module"],
+  primevue: {
+    options: {
+      theme: {
+        preset: Nora,
+      },
+    },
+  },
+});
