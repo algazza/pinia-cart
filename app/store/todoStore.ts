272ee8740ed6af0fc45bbb31a5151c69b902fa25
algazza <fadhilaswadira72564@gmail.com>
2025-12-01 14:14:12 +0700

    ✨feat: add cart pinia

---
+++ app/store/todoStore.ts
@@ -0,0 +1,40 @@
+import { defineStore } from "pinia";
+
+type TodoState = {
+  todo: string[];
+  completed: string[];
+};
+
+export const useTodoStore = defineStore("cart", {
+  state: (): TodoState => ({
+    todo: [],
+    completed: [],
+  }),
+  getters: {
+    todoLength(): number {
+      return this.todo.length;
+    },
+    completedTodoLength(): number {
+      return this.completed.length;
+    },
+  },
+  actions: {
+    addTodo(list: string) {
+      this.todo.unshift(list);
+    },
+    deleteTodo(list: string) {
+      this.todo = this.todo.filter((item) => item !== list);
+    },
+    clearTodo(){
+        this.todo = []
+    },
+    completedTodo(list: string) {
+      this.completed.unshift(list);
+      this.todo = this.todo.filter((item) => item !== list);
+    },
+    unCompletedTodo(list: string) {
+      this.todo.push(list);
+      this.completed = this.completed.filter((item) => item !== list);
+    },
+  },
+});
