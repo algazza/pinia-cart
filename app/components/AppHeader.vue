decbc22a4dda6113e2b441a5dc9d5395f623035d
algazza <fadhilaswadira72564@gmail.com>
2025-12-01 16:13:38 +0700

    💄ui: update logic onSubmit

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,16 +1,37 @@
 <script setup lang="ts">
-const value = ref("");
+import { useTodoStore } from "~/store/todoStore";
+
+const todoStore = useTodoStore();
+const value = ref<string>("");
+
+const onSubmit = () => {
+  const text = value.value.trim();
+  if (!text) return;
+
+  todoStore.addTodo(text);
+  value.value = "";
+};
 </script>
 <template>
   <header
     class="py-4 flex flex-col justify-center items-center gap-4 text-center bg-neutral-800"
   >
     <h1 class="text-2xl font-bold">To Do List</h1>
-    <form class="flex gap-2">
-      <FloatLabel variant="on" class=" p-2 border border-white rounded-full flex! gap-2!">
-        <InputText v-model="value" class="bg-transparent! focus:outline-0! border-0!"/>
+    <form @submit.prevent="onSubmit" class="flex gap-2">
+      <FloatLabel
+        variant="on"
+        class="p-2 border border-white rounded-full flex! gap-2!"
+      >
+        <InputText
+          v-model="value"
+          class="bg-transparent! focus:outline-0! border-0!"
+        />
         <label class="ml-2 bg-neutral-800!">Type new task...</label>
-        <Button icon="pi pi-plus" type="submit" class="rounded-full! font-semibold" style="font-size: 1rem"/>
+        <Button
+          icon="pi pi-plus"
+          type="submit"
+          class="rounded-full! font-semibold text-3xl"
+        />
       </FloatLabel>
     </form>
   </header>
